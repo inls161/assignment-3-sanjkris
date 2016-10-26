7285e7520a33ab3f3b3afa81d5b4028ce0f9ffbc
Sanjana <sanjkris@live.unc.edu>
2016-10-26 20:45:51 +0000

    Edited script

--- sanjkris-convert-docs.sh
+++ sanjkris-convert-docs.sh
@@ -8,16 +8,35 @@ read NAME
 #Say hello to me
 echo "Hello, $NAME! $EXCLAMATION"
 
-
+#Ask if they want to convert md to html
+echo "Do you want to convert markdown to html?"
+read $USERINPUT
+S1='Yes'
+S2="No"
 # Convert markdown to HTML
 pandoc -o justice.html justice.md
 
+#Ask if they want to convert md to docx
+echo "Do you want to convert markdown to docx?"
+read $USERINPUT
+S1='Yes'
+S2="No"
 # Convert markdown to DOCX
 pandoc -o justice.docx justice.md
 
+#Ask if they want to convert md to odt
+echo "Do you want to convert markdown to odt?"
+read $USERINPUT
+S1='Yes'
+S2="No"
 # Convert markdown to ODT
 pandoc -o justice.odt justice.md
 
+#Ask if they want to convert md to pdf
+echo "Do you want to convert markdown to pdf?"
+read $USERINPUT
+S1='Yes'
+S2="No"
 # Convert markdown to PDF
 pandoc -o justice.pdf justice.md
 
